70952d481522adfe18e2539a9302b4cf0473cc73
davzamd <davidzamd9@gmail.com>
2019-08-13 10:43:29 +0200

    Client Virtual Object Added

---
+++ src/main/java/com/david/hotelreservation/model/vo/ClientVO.java
@@ -0,0 +1,12 @@
+package com.david.hotelreservation.model.vo;
+
+import lombok.Data;
+
+@Data
+public class ClientVO {
+    private String name;
+    private String lastName;
+    private String direction;
+    private String telephoneNumber;
+    private String email;
+}
