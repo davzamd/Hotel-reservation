9eff830076dd0fe81aa5d2ddd58b56f724794c91
davzamd <davidzamd9@gmail.com>
2019-08-13 11:50:42 +0200

    Client api documentation added with swagger

--- src/main/java/com/david/hotelreservation/controller/ClientController.java
+++ src/main/java/com/david/hotelreservation/controller/ClientController.java
@@ -3,6 +3,10 @@ package com.david.hotelreservation.controller;
 import com.david.hotelreservation.model.Client;
 import com.david.hotelreservation.model.vo.ClientVO;
 import com.david.hotelreservation.service.ClientService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/client")
+@Api(tags = "cliente")
 public class ClientController {
     private final ClientService clientService;
 
@@ -19,6 +24,10 @@ public class ClientController {
     }
 
     @PostMapping
+    @ApiOperation(value = "Create Client", notes = "Service to create a new client")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201,message = "Client Successfully Created"),
+            @ApiResponse(code = 400,message = "Invalid Request")})
     public ResponseEntity<Client> createClient(@RequestBody ClientVO clientVO) {
 
         Client client = new Client();
@@ -29,6 +38,10 @@ public class ClientController {
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "Update Client", notes = "Service to update a client")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201,message = "Client Successfully Updated"),
+            @ApiResponse(code = 404,message = "Client not found")})
     public ResponseEntity<Client> updateClient(@PathVariable String id, @RequestBody ClientVO clientVO) {
         Client clientById = this.clientService.findClientById(id);
         Client updatedClient = new Client();
@@ -51,6 +64,10 @@ public class ClientController {
     }
 
     @DeleteMapping
+    @ApiOperation(value = "Delete Client", notes = "Service to delete a client")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201,message = "Client Successfully Deleted"),
+            @ApiResponse(code = 404,message = "Client not found")})
     public void removeClient(@PathVariable String id) {
         Client clientById = this.clientService.findClientById(id);
 
@@ -60,7 +77,11 @@ public class ClientController {
     }
 
     @GetMapping
-    public List<Client> findAll(){
+    @ApiOperation(value = "List Clients", notes = "Service to list all the clients")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201,message = "Clients Found"),
+            @ApiResponse(code = 404,message = "Clients not found")})
+    public List<Client> findAll() {
         return this.clientService.findAll();
     }
 
